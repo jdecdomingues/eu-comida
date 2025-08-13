2bb8fd21bd5e699bec8fded5b6e3a968e483b44c
jdecdomingues <jdecdomingues@users.noreply.github.com>
2025-08-13 14:31:26 -0300

    Aula 2

---
+++ src/main/java/com/grisaworks/eu/comida/EuComidaApplication.java
@@ -0,0 +1,13 @@
+package com.grisaworks.eu.comida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuComidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuComidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grisaworks/eu/comida/EuComidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grisaworks.eu.comida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuComidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
