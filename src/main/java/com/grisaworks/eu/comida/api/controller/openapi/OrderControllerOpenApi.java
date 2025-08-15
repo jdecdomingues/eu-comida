8bff02a6a5cecef6e8b39e5740d720735535ce19
jdecdomingues <jdecdomingues@users.noreply.github.com>
2025-08-14 21:24:19 -0300

    Aula 4

--- src/main/java/com/grisaworks/eu/comida/api/controller/openapi/OrderControllerOpenApi.java
+++ src/main/java/com/grisaworks/eu/comida/api/controller/openapi/OrderControllerOpenApi.java
@@ -1,10 +1,12 @@
 package com.grisaworks.eu.comida.api.controller.openapi;
 
-import com.grisaworks.eu.comida.domain.model.Order;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import com.grisaworks.eu.comida.domain.model.Order;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Orders")
 public interface OrderControllerOpenApi {
@@ -15,4 +17,3 @@ public interface OrderControllerOpenApi {
     @Operation(summary = "Get a status from an Order.")
     String getOrderStatus(@PathVariable Long orderId);
 }
-                    ;;;;;;;;;;;;;;;;;;;;;;;
